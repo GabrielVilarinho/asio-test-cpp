1023accbad2d1fc09a7793dce4230657c6942355
Gabriel Vilarinho <contato@gabrielvilarinho.com>
2022-11-06 21:20:43 -0300

    Created thread for ASIO's context and idle job (initial)

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,28 @@
 #include "asio/ts/internet.hpp"
 #include "lib/lazy.h"
 
-//using namespace std;
+// Since we don't know how much buffer ASIO will demand, a 20kb buffer will do
+std::vector<char> vBuffer(24 * 1024);
+
+void handleData(asio::ip::tcp::socket& socket)
+{
+    socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+        [&](std::error_code error_code_runtime, std::size_t length) {
+            if(!error_code_runtime)
+            {
+                std::cout
+                    << std::endl << std::endl
+                    << "Read " << length << " bytes"
+                    << std::endl << std::endl;
+
+                for (int i = 0; i < length; i++)
+                    std::cout << vBuffer[i];
+
+                handleData(socket);
+            }
+        }
+    );
+}
 
 int main() {
     std::cout << "src/main.cpp called" << std::endl;
@@ -20,6 +41,13 @@ int main() {
     // this space called io_context handles any specific platform
     // requirements
     asio::io_context context;
+
+
+    // Giving fake tasks to ASIO context not die/close too premature
+    asio::io_context::work idleWork(context);
+
+    // Atemporal thread for our context in ASIO
+    std::thread thrContext = std::thread([&](){ context.run(); });
 
     // ASIO needs an endpoint to request a connection see more about
     // it uses a naming convention in namespace referring to the protocol
@@ -33,7 +61,8 @@ int main() {
     //
     // endpoint as the door or the destination we're trying to reach
 //    const char *endpoint_address = "93.184.216.34"; // www.example.com
-    const char *endpoint_address = "172.217.162.206"; // www.youtube.com Brazil's mirror (SAO PAULO)
+//    const char *endpoint_address = "172.217.162.206"; // www.youtube.com Brazil's mirror (SAO PAULO)
+    const char *endpoint_address = "51.38.81.49"; //
     asio::ip::tcp::endpoint endpoint(asio::ip::make_address(endpoint_address, error_code), 80);
 
     // Socket can be described as a doorway or bridge "the means through"
@@ -41,7 +70,6 @@ int main() {
     asio::ip::tcp::socket socket1(context);
 
     // Tell the socket to try a connection
-
     socket1.connect(endpoint, error_code);
 
     if (!error_code)
@@ -56,34 +84,18 @@ int main() {
 
     if (socket1.is_open())
     {
+        handleData(socket1);
         std::string sRequest =
             "GET /index.html HTTP/1.1\r\n"
             "Host: example.com\r\n"
             "Connection: close\r\n\r\n";
 
-        // asio::ip::tcp::socket (write_some) -> Sending data to our socket/doorway
+        // asio::ip::tcp::socket (write_some) ->  Sending data to our socket/doorway
         // asio::buffer -> Buffer is just a container of an array of bytes and its length
         socket1.write_some(asio::buffer(sRequest.data(), sRequest.size()), error_code);
 
-        // asio::ip::tcp::socket (wait) -> bad because it doesn't count for the end of stream
-        // will certainly cut the stream in half when the data-stream is too big!
-        socket1.wait(socket1.wait_read);
-
-        // Gathering the data returned from the endpoint (this is an asynchronous operation)
-        size_t bytes = socket1.available();
-
-        std::cout << "Bytes available: \"" << bytes << "\"" << std::endl;
-
-        if (bytes > 0)
-        {
-            std::vector<char> vBuffer(bytes);
-
-            // asio::ip::tcp::socket (read_some) ->  Is used to read data from the stream socket
-            socket1.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), error_code);
-
-            for (auto c : vBuffer)
-                std::cout << c;
-        }
+        Lazy(5 * 1000);
     }
+
     return 0;
 }
